0a9b873b5d48b4250a0fee5b737e34f5f8822ee6
Matthew Kwong <matthewkwong2@gmail.com>
2022-05-12 07:23:58 +0800

    [Bug Fix] React import

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,3 +1,5 @@
+import React from "react"
+
 const Text = () => <div>Just a normal simple component</div>;
 
 export default Text;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { Suspense, lazy } from "react"
+import React, { Suspense, lazy } from "react"
 
 const Text = lazy(() => import("../components/Text"));
 
